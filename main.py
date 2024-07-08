d097b09b3be2cfc37a557c3a5909cad3d3aedffc
wyreanderson <wyreanderson@gmail.com>
2024-07-08 12:31:36 +0100

    screen working

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+
+#start
+pygame.init
+
+#window name
+pygame.display.set_caption("Ninja Typing")
+
+#screen
+screen = pygame.display.set_mode((400, 720))
+
+#fps
+clock = pygame.time.Clock()
+
+#loop 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    clock.tick(60)
+
+
+pygame.quit
