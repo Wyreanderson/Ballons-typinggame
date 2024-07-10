042cf4bbe548344378eb8c7aec2e3ad8c4b145b2
wyreanderson <wyreanderson@gmail.com>
2024-07-10 18:27:03 +0100

    update

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ pygame.init
 #window name
 pygame.display.set_caption("Ninja Typing")
 
+#background 
+bg = pygame.image.load("fundo.png")
+
 #screen
-screen = pygame.display.set_mode((400, 720))
+screen = pygame.display.set_mode((400, 700))
 
 #fps
 clock = pygame.time.Clock()
 
 #window loop 
 running = True
-while running:
+while running:    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    
+
     clock.tick(60)
+    
 
-
+    
 pygame.quit
